c43931ddfa8a1443f20658914d62b868b3f9f6b5
moncacon <thangnguyen28091996@gmail.com>
2022-03-16 17:06:09 +0700

    fix

---
+++ app.js
@@ -0,0 +1,57 @@
+//const value = prompt('enter');
+//const newarray = value.split(",");
+//let min = value;
+//for (let i=0; i > value.split(",").length; i++) {
+//    if(min > Number(newarray[i])){
+//        min = newarray[i];
+//    }
+//}
+//console.log(min)
+
+//--> sử dụng prompt để nhập phép tính
+//let a =Number (prompt('first'));
+//let b =Number (prompt('second'));
+//let c = alert(a+b);
+//-->
+//function sumArray(mang){
+ //   let sum = 0;
+//    for (let i = 0; i < mang.length; i++){
+//        sum += mang[i];
+//    }
+//     
+//    return sum;
+//}
+//const mang = value.split(",");
+//let mang = Number (prompt('enter'));
+//console.log(sumArray(mang));
+//--> sử dụng if,else
+/*let Number= 6;
+if(Number < 5){
+    console.log("số nhỏ hơn 5")
+}
+else if(Number > 5){
+    console.log("số lớn hơn 5")
+}*/
+//--> nhập 1 số và kiểm tra xem có phải là chẵn hay lẻ
+/*var a;
+a = prompt("Nhập vào số a", "0");
+ 
+a = parseInt(a);
+ 
+if (a % 2 == 0){
+    document.write("a là số chẵn");
+}
+else{
+    document.write("a là số lẽ");
+}*/
+//--> nhập 1 số và kiểm tra xem có phải số nguyên tố
+/*var a;
+a = prompt ("nhập số");
+a = parseInt(a);
+if ( a < 2){
+    document.write("a là số nguyên tố");
+}
+else( a > 2 % 2,4,6) 
+{
+    document.write("a không là số nguyên tố");
+}*/
